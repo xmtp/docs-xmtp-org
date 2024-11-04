8f72f21ec590c3943e3920470f5dbd659e91c112
Jennifer Hasegawa <5481259+jhaaaa@users.noreply.github.com>
2024-11-03 17:42:16 -0800

    baseline changes

--- docs/pages/protocol/v3/message-history.md
+++
@@ -1,9 +0,0 @@
----
-description: "Learn about message history concepts"
----
-
-# Message history
-
-:::info[More information]
-You can find more information on message history support in [XIP-31](https://community.xmtp.org/t/xip-31-message-history-request-system/617).
-:::
--- vocs.config.tsx
+++ vocs.config.tsx
@@ -15,16 +15,12 @@ export default defineConfig({
       </>
     );
   },
-  title: "XMTP Documentation",
+  title: "Experimnt with XMTP V3 Alpha 🧪",
   description:
-    "Documentation for XMTP, the open and secure messaging protocol for web3",
-  editLink: {
-    pattern: "https://github.com/xmtp/docs-xmtp-org/edit/main/docs/pages/:path",
-    text: "Suggest changes to this page",
-  },
+    "XMTP V3 brings support for Messaging Layer Security and much more. Use these docs to experiment with the latest V3 features.",
   logoUrl: {
-    light: "/logomark-dark-purple.png",
-    dark: "/logomark-light-purple.png",
+    light: "/logomark-light-purple-v3.png",
+    dark: "/logomark-dark-purple-v3.png",
   },
   iconUrl: "/x-mark-blue.png",
   topNav: [
@@ -47,310 +43,34 @@ export default defineConfig({
   ],
   sidebar: [
     {
-      text: "Overview",
-      collapsed: true,
-      items: [
-        {
-          text: "Intro to XMTP",
-          link: "/get-started/intro",
-        },
-        {
-          text: "FAQ",
-          link: "/get-started/faq",
-        },
-      ],
-    },
-    {
-      text: "Build chat bots with MessageKit ↗",
-      link: "https://messagekit.ephemerahq.com/",
-      items: [], // Add this line
-    },
-    {
-      text: "Embed a chat widget",
-      link: "/chat-widget/chat-widget",
-      items: [], // Add this line
-    },
-    {
-      text: "Broadcast notifications",
-      collapsed: true,
-      items: [
-        {
-          text: "Use case",
-          link: "/consent/build-engagement",
-        },
-        {
-          text: "Enable subscriptions",
-          link: "/consent/subscribe",
-        },
-        {
-          text: "Broadcast notifications",
-          link: "/consent/broadcast",
-        },
-        {
-          text: "Quickstart repos",
-          link: "/consent/consent-quickstarts",
-        },
-      ],
-    },
-    {
       text: "Build chat inboxes",
-      collapsed: true,
+      collapsed: false,
       items: [
-        {
-          text: "Get started",
-          collapsed: false,
-          items: [
-            {
-              text: "Developer quickstart",
-              link: "/get-started/developer-quickstart",
-            },
-            {
-              text: "SDKs and example apps",
-              link: "/get-started/examples",
-            },
-            {
-              text: "SDK references",
-              link: "/get-started/references",
-            },
-          ],
-        },
         {
           text: "Create a client",
-          link: "/client/create-client",
+          link: "/inboxes/create-client",
         },
         {
-          text: "Build 1:1 chat",
-          collapsed: true,
-          items: [
-            {
-              text: "Create a 1:1 conversation",
-              link: "/dms/conversations",
-            },
-            {
-              text: "Send 1:1 messages",
-              link: "/dms/messages",
-            },
-            {
-              text: "Stream 1:1 conversations & messages",
-              link: "/dms/streams",
-            },
-          ],
-        },
-        {
-          text: "Build group chat",
-          collapsed: true,
-          items: [
-            {
-              text: "Build group chat",
-              link: "/groups/build-group-chat",
-            },
-            {
-              text: "Handle group consent",
-              link: "/groups/group-consent",
-            },
-            {
-              text: "Handle group permissions",
-              link: "/groups/group-permissions",
-            },
-            {
-              text: "Handle group metadata",
-              link: "/groups/group-metadata",
-            },
-          ],
-        },
-        {
-          text: "Build push notifications",
-          collapsed: true,
-          items: [
-            {
-              text: "Send push notifications",
-              link: "/notifications/build-notifications",
-            },
-            {
-              text: "Set up a push notification server",
-              link: "/notifications/notif-server",
-            },
-            {
-              text: "Best practices",
-              link: "/notifications/notif-best-practices",
-            },
-            {
-              text: "Try Android example notifications",
-              link: "/notifications/notifs-android",
-            },
-            {
-              text: "Try iOS example notifications",
-              link: "/notifications/notifs-ios",
-            },
-          ],
+          text: "Build an inbox",
+          link: "/inboxes/build-inbox",
         },
         {
           text: "Support spam-free inboxes",
-          collapsed: true,
-          items: [
-            {
-              text: "User consent and spam-free inboxes",
-              link: "/consent/user-consent",
-            },
-            {
-              text: "Build with consent methods",
-              link: "/consent/consent-methods",
-            },
-            {
-              text: "Handle unknown contacts",
-              link: "/consent/filter-spam",
-            },
-          ],
+          link: "/inboxes/user-consent",
         },
         {
-          text: "Support content types",
-          collapsed: true,
-          items: [
-            {
-              text: "Understand content types",
-              link: "/content-types/content-types",
-            },
-            {
-              text: "Remote attachment",
-              link: "/content-types/remote-attachment",
-            },
-            {
-              text: "Replies",
-              link: "/content-types/reply",
-            },
-            {
-              text: "Reactions",
-              link: "/content-types/reaction",
-            },
-            {
-              text: "Read receipts",
-              link: "/content-types/read-receipt",
-            },
-            {
-              text: "Onchain transaction references",
-              link: "/content-types/transaction-ref",
-            },
-            {
-              text: "Custom content type",
-              link: "/content-types/custom",
-            },
-          ],
-        },
-        {
-          text: "Display Open Frames",
-          collapsed: true,
-          items: [
-            {
-              text: "Get started with Open Frames",
-              link: "/open-frames/open-frames",
-            },
-            {
-              text: "Display transactional Open Frames",
-              link: "/open-frames/transactional-open-frames",
-            },
-            {
-              text: "Display subscription Open Frames",
-              link: "/open-frames/subscription-open-frames",
-            },
-          ],
-        },
-        {
-          text: "Performance & UX",
-          collapsed: true,
-          items: [
-            {
-              text: "Use local-first architecture",
-              link: "/perf-ux/local-first",
-            },
-            {
-              text: "Use optimistic sending",
-              link: "/perf-ux/optimistic-sending",
-            },
-            {
-              text: "Resolve identities",
-              link: "/perf-ux/identity-resolution",
-            },
-            {
-              text: "Performance test",
-              link: "/perf-ux/debug-and-test",
-            },
-            {
-              text: "Launch checklist",
-              link: "/perf-ux/get-featured",
-            },
-            {
-              text: "MetaMask Snap",
-              link: "/perf-ux/xmtp-metamask-snap",
-            },
-          ],
-        },
-        {
-          text: "Troubleshoot",
-          link: "/dms/troubleshoot",
+          text: "Support push notifications",
+          link: "/inboxes/push-notifs",
         },
       ],
     },
     {
-      text: "Learn protocol concepts",
-      collapsed: true,
-      items: [
+      text: "Protocol concepts",
+      collapsed: false,
+        items: [
         {
-          text: "Account signatures",
-          link: "/protocol/signatures",
-        },
-        {
-          text: "Security and encryption",
-          link: "/protocol/security-encryption",
-        },
-        {
-          text: "Protocols",
-          link: "/protocol/protocols",
-        },
-        {
-          text: "Multi-wallet identity",
-          link: "/protocol/v3/identity",
-        },
-        {
-          text: "Message history",
-          link: "/protocol/v3/message-history",
-        },
-        {
-          text: "Smart wallet support",
-          link: "/protocol/v3/smart-wallet",
-        },
-        {
-          text: "Portable inbox",
-          link: "/protocol/portable-inbox",
-        },
-        {
-          text: "XIPs",
-          link: "/protocol/xips",
-        },
-        {
-          text: "XMTP versions",
-          link: "/protocol/xmtp-versions",
-        },
-        {
-          text: "XMTP V2",
-          collapsed: true,
-          items: [
-            {
-              text: "Architecture",
-              link: "/protocol/v2/architectural-overview",
-            },
-            {
-              text: "Key generation",
-              link: "/protocol/v2/key-generation-and-usage",
-            },
-            {
-              text: "Encryption",
-              link: "/protocol/v2/invitation-and-message-encryption",
-            },
-            {
-              text: "Algorithms in use",
-              link: "/protocol/v2/algorithms-in-use",
-            },
-          ],
+          text: "Protocol specs",
+          link: "/protocol/specs",
         },
       ],
     },
