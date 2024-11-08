ddbebf9ede8b9b9173ee9578ff9565fec418de04
Jennifer Hasegawa <5481259+jhaaaa@users.noreply.github.com>
2024-11-08 14:23:39 -0800

    add icon spaces

--- vocs.config.tsx
+++ vocs.config.tsx
@@ -43,7 +43,7 @@ export default defineConfig({
   ],
   sidebar: [
     {
-      text: "⚡️Documentation preview⚡️",
+      text: "⚡️ Documentation preview ⚡️",
       link: "", // Add this line,
       items: [ ]
     },
