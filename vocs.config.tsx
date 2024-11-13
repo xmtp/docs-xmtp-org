7a51f490741547216101f124cb2427a0bf303924
Jennifer Hasegawa <5481259+jhaaaa@users.noreply.github.com>
2024-11-12 16:48:17 -0800

    create legacy docs branch

--- vocs.config.tsx
+++ vocs.config.tsx
@@ -15,40 +15,33 @@ export default defineConfig({
       </>
     );
   },
-  title: "XMTP Documentation",
+  title: "Legacy XMTP documentation",
   description:
-    "Documentation for XMTP, the open and secure messaging protocol for web3",
-  editLink: {
-    pattern: "https://github.com/xmtp/docs-xmtp-org/edit/main/docs/pages/:path",
-    text: "Suggest changes to this page",
-  },
+    "This site provides documentation for legacy versions of XMTP. To learn how to build 1:1 and group chat experiences with the latest version of XMTP, see https://docs.xmtp.org/.", 
   logoUrl: {
-    light: "/logomark-dark-purple.png",
-    dark: "/logomark-light-purple.png",
+    light: "/xmtp-logo-lockup-grey-black.png",
+    dark: "/xmtp-logo-lockup-grey-white.png",
   },
-  iconUrl: "/x-mark-blue.png",
+  iconUrl: "/xmtp-logo-grey.png",
   topNav: [
     {
-      text: "Support 💬",
-      link: "https://converse.xyz/group-invite/a0XKzl9oVpWNXcuYDZLMf",
+      text: "🔔 Read the latest docs",
+      link: "https://docs.xmtp.org/",
     },
-    { text: "XMTP.org", link: "https://xmtp.org/" },
   ],
   ogImageUrl: {
     "/": "/xmtp-og-card.jpeg",
     "/docs":
       "https://vocs.dev/api/og?logo=%logo&title=%title&description=%description",
   },
-  socials: [
-    {
-      icon: "github",
-      link: "https://github.com/xmtp",
-    },
-  ],
   sidebar: [
     {
-      text: "⚡️ Preview XMTP V3 docs ⚡️",
-      link: "/preview",
+      text: "🔔 Read the latest docs ↗",
+      link: "https://docs.xmtp.org/",
+      items: [], // Add this line
+    },
+    {
+      text: "⎯ LEGACY DOCUMENTATION ⎯",
       items: [], // Add this line
     },
     {
@@ -64,11 +57,6 @@ export default defineConfig({
           link: "/get-started/faq",
         },
       ],
-    },
-    {
-      text: "Build chat bots with MessageKit ↗",
-      link: "https://messagekit.ephemerahq.com/",
-      items: [], // Add this line
     },
     {
       text: "Embed a chat widget",
