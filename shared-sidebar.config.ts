aee97333997d64175592a55122e70d943624c6c2
fabri <fguespe@gmail.com>
2025-10-01 12:45:30 -0300

    Update sidebar text for consistency and clarity

--- shared-sidebar.config.ts
+++ shared-sidebar.config.ts
@@ -106,7 +106,7 @@ export const sidebarConfig = {
           link: "/agents/content-types/transactions",
         },
         {
-          text: "Transaction reference",
+          text: "Transaction references",
           link: "/agents/content-types/transaction-refs",
         },
       ],
